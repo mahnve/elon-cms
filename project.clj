d46b7c9b652e4a14618a105b0b9a007f6a61b192
Marcus Ahnve <mahnve@gammelsmurf>
2016-07-09 00:49:00 +0200

    BDU library additions

--- project.clj
+++ project.clj
@@ -6,7 +6,17 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [yada "1.1.25"]
                  [aleph "0.4.1"]
-                 [bidi "2.0.9"]]
+                 [bidi "2.0.9"]
+                 [com.taoensso/timbre "4.5.1"]
+                 [hiccup "1.0.5"]
+                 [com.layerware/hugsql "0.4.7"]
+                 [h2 "0.2.0"]
+                 [mount "0.1.10"]
+                 [prismatic/schema "1.1.2"]
+                 [aero "1.0.0"]
+                 [honeysql "0.7.0"]
+                 [clj-aws-s3 "0.3.10"]
+                 [fivetonine/collage "0.2.1"]]
   :main ^:skip-aot elon-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
