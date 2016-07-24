a95f3d4b92f888a6533b3cdd3b4e2a220c5cdbf6
Marcus Ahnve <mahnve@gammelsmurf>
2016-07-24 23:30:53 +0200

    Update to project.clj
    * Updated yada version, hopefully fixes panics
    * Description and url, yay
    * Nice to have libraries ...

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject elon-clj "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A small headless CMS"
+  :url "http://github.com/mahnve/elon-cms"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [yada "1.1.25"]
+                 [yada "1.1.28"]
                  [aleph "0.4.1"]
                  [bidi "2.0.9"]
                  [com.taoensso/timbre "4.5.1"]
@@ -16,7 +16,10 @@
                  [aero "1.0.0"]
                  [honeysql "0.7.0"]
                  [clj-aws-s3 "0.3.10"]
-                 [fivetonine/collage "0.2.1"]]
+                 [fivetonine/collage "0.2.1"]
+                 [joplin "0.3.6"]
+                 [ragtime "0.6.1"]
+                 [com.soundcloud/prometheus-clj "2.4.0"]]
   :main ^:skip-aot elon-clj.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
